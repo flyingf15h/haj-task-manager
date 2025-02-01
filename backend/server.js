c486bb1ea1321c6374dbd0b1001d9722f2e7083e
tszfeiw <fei.wang3@student.tdsb.on.ca>
2025-01-31 19:16:51 -0500

    updated stuff

--- backend/server.js
+++ backend/server.js
@@ -102,6 +102,6 @@ app.delete("/tasks/:id", async (req, res) => {
   }
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 8080
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
